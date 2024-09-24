f2c30a81c1034a29cca352b49313680bda3c352a
Čapčuch, Patrik - Student Rijn IJssel <00317034@rijnijssel.nl>
2024-09-24 09:32:13 +0200

    Login

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,29 +1,43 @@
-@props(['id'])
+@props(['id', 'maxWidth'])
 
-<div id="{{ $id }}" aria-hidden="false"
-     class="absolute w-screen h-screen top-0 left-0 transition-opacity aria-hidden:opacity-100 opacity-0
-     aria-hidden:z-50 -z-50"
+@php
+    $id = $id ?? md5($attributes->wire('model'));
+
+    $maxWidth = [
+        'sm' => 'sm:max-w-sm',
+        'md' => 'sm:max-w-md',
+        'lg' => 'sm:max-w-lg',
+        'xl' => 'sm:max-w-xl',
+        '2xl' => 'sm:max-w-2xl',
+    ][$maxWidth ?? '2xl'];
+@endphp
+
+<div
+    x-data="{ show: @entangle($attributes->wire('model')) }"
+    x-on:close.stop="show = false"
+    x-on:keydown.escape.window="show = false"
+    x-show="show"
+    id="{{ $id }}"
+    class="jetstream-modal fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
+    style="display: none;"
 >
-    <div class="w-full h-full bg-slate-900 opacity-75">
-
-    </div>
-    <div class="absolute top-0 left-0 w-full flex justify-center">
-        <div id="modal_slot" {{ $attributes->merge([
-        'class' => "lg:max-w-2xl w-full bg-gray-800 rounded-lg px-2 py-4 mt-32"
-        ]) }}>
-            {{ $slot }}
-        </div>
+    <div x-show="show" class="fixed inset-0 transform transition-all" x-on:click="show = false" x-transition:enter="ease-out duration-300"
+         x-transition:enter-start="opacity-0"
+         x-transition:enter-end="opacity-100"
+         x-transition:leave="ease-in duration-200"
+         x-transition:leave-start="opacity-100"
+         x-transition:leave-end="opacity-0">
+        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
     </div>
 
-    <script>
-        {
-            const modal = document.getElementById("{{ $id }}");
-
-            modal.addEventListener("click", (event) => {
-                if (event.target?.id === "modal_slot") return;
-
-                modal.setAttribute("aria-hidden", "false");
-            });
-        }
-    </script>
+    <div x-show="show" class="mb-6 bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }} sm:mx-auto"
+         x-trap.inert.noscroll="show"
+         x-transition:enter="ease-out duration-300"
+         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+         x-transition:leave="ease-in duration-200"
+         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+         x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+        {{ $slot }}
+    </div>
 </div>
