d1415010e2956313a51fbbec4c29b7eab80f29e2
Patrik <00317034@rijnijssel.nl>
2024-09-24 10:22:53 +0200

    update frontend: Made the table look a bit nicer

--- resources/views/manage/admin-overview.blade.php
+++ resources/views/manage/admin-overview.blade.php
@@ -25,58 +25,63 @@
                 </a>
             </div>
 
-            <table class="w-full border-gray-600 border-[2px] mt-4">
-                <thead class="bg-zinc-700">
-                <tr class="text-left">
-                    <th class="p-1 border-gray-600 border-[2px]">Naam</th>
-                    <th class="p-1 border-gray-600 border-[2px]">Email</th>
-                    <th class="p-1 border-gray-600 border-[2px]">Functie</th>
-                    <th class="p-1 border-gray-600 border-[2px]">Acties</th>
-                </tr>
-                </thead>
-                <tbody class="bg-gray-700">
-                <?php $i = 0; ?>
-                @foreach($users as $user)
-                    @if($user->role === 'user')
-                        @continue
-                    @endif
+            <br />
+
+            <div class="overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <td class="p-1 border-gray-600 border-[2px]">{{ $user->firstname}} {{$user->middlename}} {{$user->lastname}}</td>
-                        <td class="p-1 border-gray-600 border-[2px]">{{ $user->email }}</td>
-                        <td class="p-1 border-gray-600 border-[2px]">{{ $user->role }}</td>
-                        <td class="p-1  border-gray-600 border-[2px] w-min">
-                            <x-modal-button for="modal-delete-{{$i}}"
-                                            class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md
+                        <th class="px-6 py-3">Naam</th>
+                        <th class="px-6 py-3">Email</th>
+                        <th class="px-6 py-3">Functie</th>
+                        <th class="px-6 py-3">Acties</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php $i = 0; ?>
+                    @foreach($users as $user)
+                        @if($user->role === 'user')
+                            @continue
+                        @endif
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->firstname}} {{$user->middlename}} {{$user->lastname}}</td>
+                            <td class="px-6 py-4">{{ $user->email }}</td>
+                            <td class="px-6 py-4">{{ ucfirst($user->role) }}</td>
+                            <td class="px-6 py-4">
+                                <x-modal-button for="modal-delete-{{$i}}"
+                                                class="inline-flex items-center justify-center px-4 py-2 bg-red-600 border border-transparent rounded-md
                                                         font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700
                                                         focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800
                                                         transition ease-in-out duration-150 disabled:opacity-50"
-                                            :disabled="auth()->user()->id === $user->id">
-                                Verwijder
-                            </x-modal-button>
-                        </td>
-                    </tr>
+                                                :disabled="auth()->user()->id === $user->id">
+                                    Verwijder
+                                </x-modal-button>
+                            </td>
+                        </tr>
 
-                    @if(auth()->user() !== $user)
-                        <x-admin-modal id="modal-delete-{{$i}}" class="px-8">
-                            <h1 class="text-4xl">Account verwijderen</h1>
-                            <p class="text-lg mt-4">Weet je zeker dat je: {{ $user->firstname }} wilt verwijderen?</p>
-                            <form method="POST" action="{{ route('manage.admins.delete', $user) }}">
-                                @method('DELETE')
-                                @csrf
-                                <x-danger-button type="submit" class="float-end">
-                                    Verwijderen
-                                </x-danger-button>
-                            </form>
-                            <x-button class="float-end mr-2" @click="closeModal('modal-delete-{{$i}}')">
-                                Annuleren
-                            </x-button>
-                        </x-admin-modal>
-                            <?php $i++; ?>
-                    @endif
+                        @if(auth()->user() !== $user)
+                            <x-admin-modal id="modal-delete-{{$i}}" class="px-8">
+                                <h1 class="text-4xl">Account verwijderen</h1>
+                                <p class="text-lg mt-4">Weet je zeker dat je: {{ $user->firstname }} wilt verwijderen?</p>
+                                <form method="POST" action="{{ route('manage.admins.delete', $user) }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <x-danger-button type="submit" class="float-end">
+                                        Verwijderen
+                                    </x-danger-button>
+                                </form>
+                                <x-button class="float-end mr-2" @click="closeModal('modal-delete-{{$i}}')">
+                                    Annuleren
+                                </x-button>
+                            </x-admin-modal>
+                                <?php $i++; ?>
+                        @endif
 
-                @endforeach
-                </tbody>
-            </table>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
